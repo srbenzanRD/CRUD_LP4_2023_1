e03a7c89d86506d0a75154735a7be0ecf0fe1225
Felix Benzan <felixbenz07@gmail.com>
2023-02-03 19:21:01 -0400

    prueba de commit

--- Server/Models/Usuario.cs
+++ Server/Models/Usuario.cs
@@ -5,6 +5,7 @@ namespace CRUD.Server.Models;
 //Representara la estuctura de datos del usuario. (La Tabla en la DB).
 public class Usuario
 {
+    
     [Key]
     public int Id { get; set; }
     public int UsuarioRolId { get; set; }
